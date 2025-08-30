e06078d3f0d4dab6bdbd340a8dbaf5b8c9c2013c
tauqeerrumaney <61836057+tauqeerrumaney@users.noreply.github.com>
2025-08-30 19:45:34 +0200

    Update models.py

--- start/models.py
+++ start/models.py
@@ -1,9 +1,9 @@
 from otree.api import *
 
-class C(BaseConstants):
-    NAME_IN_URL = __name__.split('.')[-2]  # auto-derive app name from folder
-    PLAYERS_PER_GROUP = None
-    NUM_ROUNDS = 1
+class Constants(BaseConstants):  # NOT C(BaseConstants)
+    name_in_url = 'start'        # lowercase
+    players_per_group = None
+    num_rounds = 7              # or however many practice pages
 
 class Subsession(BaseSubsession):
     pass
@@ -12,4 +12,14 @@ class Group(BaseGroup):
     pass
 
 class Player(BasePlayer):
-    pass
+    # Your practice fields here
+    practice_1_response = models.StringField(blank=True)
+    # Add more fields...
+
+# Add your practice pages here
+class PracticePage1(Page):
+    form_model = 'player'
+    form_fields = ['practice_1_response']
+
+# Add page_sequence
+page_sequence = [PracticePage1]  # Add all your pages
