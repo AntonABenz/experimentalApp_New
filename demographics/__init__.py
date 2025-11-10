1646c25ac90a1adfc5564667c2f6d9481bc30ea8
tauqeerrumaney <61836057+tauqeerrumaney@users.noreply.github.com>
2025-11-10 13:01:23 +0530

    Delete demographics directory

--- demographics/__init__.py
+++
@@ -1,6 +0,0 @@
-
-from .models import *  # noqa
-try:
-    from .pages import *  # noqa
-except Exception:
-    pass
--- demographics/pages.py
+++
@@ -1,8 +0,0 @@
-from otree.api import *
-from . import *
-
-class Demographics(Page):
-    form_model = 'player'
-    form_fields = ['age', 'gender', 'education']  # Add your demographic fields
-
-page_sequence = [Demographics]
