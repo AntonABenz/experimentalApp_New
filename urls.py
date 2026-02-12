14166d75e982a15c98e726c50ff502b321f3feab
tauqeerrumaney <61836057+tauqeerrumaney@users.noreply.github.com>
2026-02-12 17:12:37 +0100

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.urls import path, include
 from prolific_webhook import prolific_webhook
 
 urlpatterns = [
     path("prolific/webhook/", prolific_webhook),
+    path("", include("otree.urls")),
 ]
