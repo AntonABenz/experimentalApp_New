60f4c5e69d49a524c9a408b38983b5f68eba3b35
tauqeerrumaney <61836057+tauqeerrumaney@users.noreply.github.com>
2026-02-16 21:34:34 +0100

    Update urlpatterns to include prolific webhook path

--- urls.py
+++ urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from otree.urls import urlpatterns
+import otree.urls
+
 from prolific_webhook import prolific_webhook_view
 
-urlpatterns += [
+urlpatterns = otree.urls.urlpatterns + [
     path("prolific/webhook/", prolific_webhook_view, name="prolific_webhook"),
 ]
