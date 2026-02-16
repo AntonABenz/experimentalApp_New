63ece88ab98a38b953d807f3de88ade25ef83131
tauqeerrumaney <61836057+tauqeerrumaney@users.noreply.github.com>
2026-02-16 21:47:56 +0100

    Update urls.py

--- urls.py
+++ urls.py
@@ -3,6 +3,6 @@ import otree.urls
 
 from prolific_webhook import prolific_webhook_view
 
-urlpatterns = otree.urls.urlpatterns + [
+urlpatterns = otree.urls.get_urlpatterns() + [
     path("prolific/webhook/", prolific_webhook_view, name="prolific_webhook"),
 ]
