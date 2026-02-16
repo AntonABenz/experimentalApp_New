f781a6091113f0e495c169f51fdc0dfcec52c222
tauqeerrumaney <61836057+tauqeerrumaney@users.noreply.github.com>
2026-02-16 21:09:25 +0100

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
+from otree.urls import urlpatterns
 from prolific_webhook import prolific_webhook_view
 
-urlpatterns = [
-    # Prolific will call this URL
+urlpatterns += [
     path("prolific/webhook/", prolific_webhook_view, name="prolific_webhook"),
 ]
