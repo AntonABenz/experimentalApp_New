66ebe29220d77ff7c7aab71029c2cf2e6cdf3896
tauqeerrumaney <61836057+tauqeerrumaney@users.noreply.github.com>
2026-02-16 22:43:03 +0100

    Refactor URL routing and add ping endpoint

--- urls.py
+++ urls.py
@@ -1,8 +1,14 @@
-import otree.urls
+# urls.py
+from otree.urls import get_urlpatterns
 from starlette.routing import Route
+from starlette.responses import PlainTextResponse
 
 from prolific_webhook import prolific_webhook_view
 
-urlpatterns = otree.urls.get_urlpatterns() + [
-    Route("/prolific/webhook/", prolific_webhook_view, methods=["POST", "GET"]),
+def ping(request):
+    return PlainTextResponse("ok", status_code=200)
+
+urlpatterns = get_urlpatterns() + [
+    Route("/prolific/ping/", endpoint=ping, methods=["GET"]),
+    Route("/prolific/webhook/", endpoint=prolific_webhook_view, methods=["POST", "GET"]),
 ]
