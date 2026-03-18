407bb448e39c3c9c51abc804ff550daf2575eda3
AntonABenz <48247568+AntonABenz@users.noreply.github.com>
2026-03-18 12:37:31 +0100

    Update __init__.py
    def vars_for_template(player), l. 1397

--- img_desc/__init__.py
+++ img_desc/__init__.py
@@ -1395,11 +1395,13 @@ class CaptureProlificID(Page):
 
     @staticmethod
     def vars_for_template(player):
+        p = player.participant
         return dict(
-            prolific_pid=player.request.GET.get("PROLIFIC_PID", ""),
-            study_id=player.request.GET.get("STUDY_ID", ""),
-            session_id=player.request.GET.get("SESSION_ID", ""),
+            prolific_pid=p.vars.get("prolific_id", ""),
+            study_id=p.vars.get("study_id", ""),
+            session_id=p.vars.get("session_id", ""),
         )
+
 
     @staticmethod
     def before_next_page(player, timeout_happened):
